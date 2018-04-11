c7736ca36f6b6c608df72d23fe388fafcd3d5044
UnclePluto <dypluto@163.com>
2018-04-11 22:49:01 +0800

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,12 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StaticPagesController@home');
+
+Route::get('/home', 'StaticPagesController@home');
+
+Route::get('/about', 'StaticPagesController@about');
+
+Route::get('/help', 'StaticPagesController@help');
